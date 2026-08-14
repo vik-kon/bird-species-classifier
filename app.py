0aa8cc4fa3406e26f71d2ea8aa2150baab3f6ef7
Vikram Kondagunta <96449200+vik-kon@users.noreply.github.com>
2026-08-14 17:58:39 -0500

    cut the model weight

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask_cors import CORS
 import torch
-import sys
-sys.path.append("src")
+import torchvision
 from torchvision import transforms
 from PIL import Image
 from flask import Flask, request, jsonify, send_from_directory
-from model import get_model
+from huggingface_hub import hf_hub_download
 import os
 
 class_name = []
@@ -13,10 +12,9 @@ with open("classes.txt") as f:
     for line in f:
         class_name.append(line.strip().split()[1])
 
-
-model = get_model()
-from huggingface_hub import hf_hub_download
 model_path = hf_hub_download(repo_id="vvkon/bird-species-classifier", filename="model.pth")
+model = torchvision.models.resnet18(weights=None)
+model.fc = torch.nn.Linear(512, 200)
 model.load_state_dict(torch.load(model_path, map_location="cpu"))
 model.eval()
 
@@ -28,26 +26,25 @@ data_transforms = transforms.Compose([
         std = [0.229, 0.224, 0.225],
     ),
 ])
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def index():
     return send_from_directory(os.path.dirname(os.path.abspath(__file__)), "index.html")
-@app.route("/predict", methods=["POST"])
 
+@app.route("/predict", methods=["POST"])
 def predict():
-    #logic
     file = request.files["image"]
     img = Image.open(file).convert("RGB")
     img = data_transforms(img)
-    img= img.unsqueeze(0)
-    with torch.no_grad():       
+    img = img.unsqueeze(0)
+    with torch.no_grad():
         output = model(img)
         predicted_idx = torch.argmax(output, dim=1).item()
-        confidence = torch.softmax(output, dim = 1)[0][predicted_idx].item()
+        confidence = torch.softmax(output, dim=1)[0][predicted_idx].item()
         species = class_name[predicted_idx]
-
     return jsonify({"species": species, "confidence": confidence})
 
 if __name__ == "__main__":
